a9a5d0377ac2622aedf06612d02e54d3a933fdf8
Jason Read <jason@cloudharmony.com>
2017-04-02 16:39:47 -0700

    initial implementation

--- lambda/main.py
+++
@@ -1,8 +0,0 @@
-import logging, subprocess
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-def handler(event, context):    
-  # return subprocess.check_output(['./dig', '@8.8.8.8', 'google.com'])
-  logger.info(subprocess.check_output('cd bin && ./test1.sh', shell=True))
-  return True
